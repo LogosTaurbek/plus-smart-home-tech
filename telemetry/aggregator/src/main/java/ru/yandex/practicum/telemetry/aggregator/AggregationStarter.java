8f9aca97f8351778b806a53a73b167ec74c0a3c3
m.taurbek <m.taurbek@alem.kz>
2026-06-06 12:59:21 +0500

    fixes

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/AggregationStarter.java
@@ -93,6 +93,7 @@ public class AggregationStarter {
                         .build()
         );
 
+
         SensorStateAvro oldState = snapshot.getSensorsState().get(sensorId);
 
         if (oldState != null) {
